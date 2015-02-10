bb5f640e92b7adde8f53f1f19bb6bf22047f4db8
JohanDy <540195@student.hin.no>
2015-02-10 12:53:11 +0100

    Ny oppdatering

--- Test/TestGIT/src/Test.java
+++ Test/TestGIT/src/Test.java
@@ -2,5 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Norway! Ill be your guide...");
+		System.out.println("This is update");
+		System.out.println("Heisan oppdatering"); //Oppdatert
 	}
 }
